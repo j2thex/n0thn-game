5ce852506bb9d0d8596d7b89eb66503c6bb59f17
j2thex <jnamed@gmail.com>
2023-09-25 17:48:37 +0200

    index.html

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Serve static files (like your game's assets, CSS, JS, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to serve the game's main page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
