6552f681b752303f7d578787e00b85c9bdcbae6a
j2thex <jnamed@gmail.com>
2023-09-25 23:57:34 +0200

    some improvements

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIo = require('socket.io');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,39 +14,38 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
-
-const http = require('http');
-const socketIo = require('socket.io');
-
-
+// Create the server and bind both Express and socket.io to it
 const server = http.createServer(app);
 const io = socketIo(server);
+
 let players = [];
 io.on('connection', (socket) => {
     console.log('a user connected');
+    console.log('a user connected with socket id:', socket.id);
+
     if (players.length < 2) {
         players.push(socket.id);
         socket.emit('assignCharacter', players.length === 1 ? 'character1' : 'character2');
     }
-    
+
     socket.on('moveCharacter', (data) => {
-        // Broadcast the movement data to other clients
+        console.log('Received movement data on server:', data);
         socket.broadcast.emit('characterMoved', data);
     });
-    
+
+
     // Handle movement events here
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
+        const index = players.indexOf(socket.id);
+        if (index > -1) {
+            players.splice(index, 1);
+        }
     });
 });
 
+// Start the server
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
--- public/game.js
+++ public/game.js
@@ -1,5 +1,14 @@
 const SERVER_URL = window.location.hostname === "localhost" ? "http://localhost:3000" : "https://n0thn-76d921721851.herokuapp.com/";
 const socket = io.connect(SERVER_URL);
+
+socket.on('connect', () => {
+    console.log('Connected to the server with socket id:', socket.id);
+});
+socket.on('assignCharacter', (character) => {
+    assignedCharacter = character;
+    console.log('Assigned character:', assignedCharacter);
+});
+
 const config = {
     type: Phaser.AUTO,
     width: 800,
@@ -11,7 +20,12 @@ const config = {
     }
 };
 
-const game = new Phaser.Game(config);
+document.getElementById('startGameButton').addEventListener('click', function () {
+    // Prevent multiple game instances
+    if (!window.gameInstance) {
+        window.gameInstance = new Phaser.Game(config);
+    }
+});
 
 function preload() {
     this.load.image('character1', 'dude.png');
@@ -27,17 +41,30 @@ function create() {
     char1 = this.add.sprite(100, 300, 'character1');
     char2 = this.add.sprite(700, 300, 'character2');
 
-    // Set up the keyboard input listener
-    this.input.keyboard.on('keydown', (event) => {
-        let movementData = {
-            character: assignedCharacter,
-            direction: event.key
-        };
-        socket.emit('moveCharacter', movementData);
-    });
+  // Set up the keyboard input listener
+this.input.keyboard.on('keydown', (event) => {
+    if (!assignedCharacter) {
+        console.warn('Character not assigned yet.');
+        return;
+    }
+    
+    let movementData = {
+        character: assignedCharacter,
+        direction: event.key
+    };
+    console.log('Sending movement data to server:', movementData);
+    socket.emit('moveCharacter', movementData);
+
+    // Prevent the default action (scrolling) for arrow keys
+    if (['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(event.key)) {
+        event.preventDefault();
+    }
+});
+
 
     // Set up the socket listener for character movement
     socket.on('characterMoved', (data) => {
+        console.log('Received movement data:', data);
         if (data.character === 'character1') {
             char1Movement = data.direction;
         } else {
@@ -46,32 +73,38 @@ function create() {
     });
 
     socket.on('assignCharacter', (character) => {
+        console.log('Assigned character:', character);
         assignedCharacter = character;
     });
 }
 
 function update() {
-    // Handle character1 movement
-    if (char1Movement) {
-        if (char1Movement === 'ArrowUp') {
-            char1.y -= 5;
-        } else if (char1Movement === 'ArrowDown') {
-            char1.y += 5;
-        } // ... handle other directions
 
+    if (char1Movement) {
+        handleMovement(char1, char1Movement);
         char1Movement = null;
     }
 
     // Handle character2 movement
     if (char2Movement) {
-        if (char2Movement === 'ArrowUp') {
-            char2.y -= 5;
-        } else if (char2Movement === 'ArrowDown') {
-            char2.y += 5;
-        } // ... handle other directions
-
+        handleMovement(char2, char2Movement);
         char2Movement = null;
     }
+}
 
-    // ... (other game loop logic)
+function handleMovement(character, direction) {
+    switch (direction) {
+        case 'ArrowUp':
+            character.y -= 5;
+            break;
+        case 'ArrowDown':
+            character.y += 5;
+            break;
+        case 'ArrowLeft':
+            character.x -= 5;
+            break;
+        case 'ArrowRight':
+            character.x += 5;
+            break;
+    }
 }
